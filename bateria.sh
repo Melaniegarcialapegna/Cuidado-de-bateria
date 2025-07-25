37ee99e627dcfad290b7288ec0dd299462e9ebe9
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2025-07-25 00:03:05 -0300

    Actualizar script

--- bateria.sh
+++ bateria.sh
@@ -10,10 +10,10 @@ NOMBRE=$(upower -e | grep BAT)
 PORCENTAJE=$(upower -i "$NOMBRE" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
 
 #Se guarda estado de bateria
-STATE=$(upower -i "$NOMBRE" | grep -E "state" | awk '{print $2}')
+ESTADO=$(upower -i "$NOMBRE" | grep -E "state" | awk '{print $2}')
 
 #Si se esta cargando y ademas la bateria es mayor o igual a 78 -> envia notificacion
-if [ "$STATE" = "charging" ] && [ "$PORCENTAJE" -ge 78 ] ; then
+if [ "$ESTADO" = "charging" ] && [ "$PORCENTAJE" -ge 78 ] ; then
     notify-send -u critical "🔋 Bateria al $PORCENTAJE%" "Desconecta el cargador para preservar la vida util de tu dispositivo."
     canberra-gtk-play -i battery-low -d "BateriaLlena" &
 fi
